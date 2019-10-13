41d1269d4970d374ec1170dda7de166d9587f03f
Alice Freda <alicefreda@gmail.com>
2019-10-13 22:29:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 def square_array(array)
+<<<<<<< HEAD
   squared = []
   array.each {|number| squared  << (number ** 2)}
   squared
+=======
+  array.each do |number|
+    squared = array << number ** 2
+  end
+>>>>>>> 24b98373afe9b4a10a60d962b7108403b6266014
 end
